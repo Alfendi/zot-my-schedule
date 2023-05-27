4f789d2145aaf10fc1b72cc6f92a292d72f1e160
Alfendi <tenzosc@uci.edu>
2023-05-27 16:24:46 -0700

    combined work with Brandon

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__, template_folder="template", static_folder='static')
 
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
